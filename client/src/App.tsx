f99155f513a372e206e3fcc13fc58934b3f01ace
Robin van den Berg <robinofski.development@gmail.com>
2024-12-18 15:40:38 +0100

    chore: deno fmt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import Index from "./pages/Index.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<Index />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
       </QueryClientProvider>
     </BrowserRouter>
   );
--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,17 +1,17 @@
 type Expense = {
-    id: number,
-    date: string,
-    description: string,
-    amount: number,
-    category: string
-}
+  id: number;
+  date: string;
+  description: string;
+  amount: number;
+  category: string;
+};
 
 const Index = () => {
-    return (
-        <div>
-            <h1>Expenses</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Expenses</h1>
+    </div>
+  );
 };
 
 export default Index;
