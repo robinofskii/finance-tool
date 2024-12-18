62777171a7091c8fd79af39d9dd597846099c378
Robin van den Berg <robinofski.development@gmail.com>
2024-12-18 15:33:21 +0100

    feat: add CORS support and a sample API endpoint

--- server/main.ts
+++ server/main.ts
@@ -1,11 +1,20 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
+import { oakCors } from "@tajpouria/cors";
 import routeStaticFilesFrom from "./util/routeStaticFilesFrom.ts";
 
 export const app = new Application();
 const router = new Router();
 
+router.get("/api/", (context) => {
+  context.response.body = {
+    data: "Hello, World!",
+  };
+});
+
+app.use(oakCors());
 app.use(router.routes());
+app.use(router.allowedMethods());
 app.use(routeStaticFilesFrom([
   `${Deno.cwd()}/client/dist`,
   `${Deno.cwd()}/client/public`,
