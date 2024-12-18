4c5c5dc70bd3d5c6b2b2ab7f06421dc7100182ab
Robin van den Berg <robinofski.development@gmail.com>
2024-12-18 15:32:46 +0100

    refactor: remove unused CSS and restructure App component with routing

---
+++ client/src/pages/Index.tsx
@@ -0,0 +1,17 @@
+type Expense = {
+    id: number,
+    date: string,
+    description: string,
+    amount: number,
+    category: string
+}
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Expenses</h1>
+        </div>
+    );
+};
+
+export default Index;
